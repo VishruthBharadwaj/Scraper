9c645987940984910156da1b397a6d8ffdd532da
VishruthBharadwaj <64675356+VishruthBharadwaj@users.noreply.github.com>
2021-02-04 11:14:03 +0530

    Update twitterkeys.py

--- twitterkeys.py
+++ twitterkeys.py
@@ -1,8 +1,8 @@
 twitter = {
-			"consumer_key" : "sAcCBILBdhdfNuRmFuzZFZLCK",
-			"consumer_secret" : "crrWltPZt3cCv1G8RzCRDlRYPemWYWQgIReGJAAniOU4pOL4fn",
-			"access_key" : "1353293981384232965-QKR7IaxJoUfOkAwmQkCQC9PuSsihYj",
-			"access_secret" : "TJny0NFXeUH31mmthW55j5J3EjBZvzaeMbloo2xYKRTG4"
+			"consumer_key" : "Please enter you key here",
+			"consumer_secret" :"Please enter you key here",
+			"access_key" : "Please enter you key here",
+			"access_secret" : "Please enter you key here"
 				
 			}
 
